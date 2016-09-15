4ab717aa786ee50742c01b3ac36700c48d482592
Edd Brown <edd_brown@bloomfield.edu>
2016-09-15 19:31:40 -0400

    FirstSim

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody2D rb;
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (h, v);
+
+		if(Input.GetKey(KeyCode.B))
+			rb.AddForce (movement);
+	}
+}
---
+++ Assets/MoveBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 movement = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		if(Input.GetKey(KeyCode.Alpha1))
+			transform.position += movement;
+
+
+
+	}
+}
